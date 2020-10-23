7838dad5d8afdb6cdb070acaa22d24142080ba3d
Eduardo <thiago.novak7@gmail.com>
2020-10-23 00:22:21 -0300

    Add, ternario

---
+++ sesion01/desafio01/calc.js
@@ -0,0 +1,21 @@
+/*
+Regras: não usar if and else, 
+
+A calculadora deve calculsar três possibilidades de gorjeta
+
+15% se o valor for entre 50 e 300.
+20% se o valor não estiver entre 50 e 300
+*/
+
+
+function calcGorjeta(gorjeta){
+    gorjeta <= 300 && gorjeta >= 50 ?   console.log('A Gorjeta será de 15%') : console.log('A gorjeta será de 20%') ;
+
+    // console.log(typeof gorjeta)
+}
+
+calcGorjeta(1);
+
+
+
+
