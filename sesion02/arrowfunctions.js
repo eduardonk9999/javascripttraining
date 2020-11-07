da055bbabcfab6ae8c51b56794bf954576ce5a6d
eduardosilva <fulanodetal@exemplo.br>
2020-11-07 18:59:20 -0300

    Add, aulas functions

---
+++ sesion02/arrowfunctions.js
@@ -0,0 +1,16 @@
+// Functions expressions
+const calcAge2 = function (birthYeah) {
+    return 2037 - birthYeah;
+}
+calcAge2(1992);
+
+// Arrow function
+const calcAge3 = birthYeah => 2037 - birthYeah;
+calcAge3(1992);
+
+
+const yearsUntilRetirment = birthYeah => {
+    const age = 2037 - birthYeah;
+    const retirement = 65 - age;
+    return retirement;
+}
